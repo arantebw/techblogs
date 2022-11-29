b2961996af1ff78c859704eb742ddadb94c7e4f5
Billy Arante <arantebillywilson@gmail.com>
2022-11-29 09:53:54 +0800

    feat(license): add a Creative Commons license

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,11 +7,20 @@ const headingStyles = {};
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Billy Arante's Techblogs
-      </h1>
-    </main>
+    <>
+      <main style={pageStyles}>
+        <h1 style={headingStyles}>
+          Billy Arante's Techblogs
+        </h1>
+      </main>
+      <footer>
+        <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">
+          <img alt="Creative Commons License" style={{borderWidth: "0"}} src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" />
+        </a>
+        <br />
+        This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
+      </footer>
+    </>
   )
 }
 
