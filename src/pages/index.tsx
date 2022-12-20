bda4a5c34d1db47707ed92c77758a643206e227f
Billy Arante <arantebillywilson@gmail.com>
2022-12-20 15:19:49 +0800

    refactor(index.tsx): add a header line

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ const IndexPage: React.FC<PageProps> = () => {
   return (
     <Layout>
       <main>
+        <h1>List of my posts</h1>
         <section>
           <h3>Post #1</h3>
           <p>The quick brown fox jumps over the lazy dog near the river bank.</p>
