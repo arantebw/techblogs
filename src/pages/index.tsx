99c425f7df7ab9a642b2d78f91387f04ca943e17
Billy Arante <arantebillywilson@gmail.com>
2022-12-22 20:01:45 +0800

    feat(posts): ✨ layout the post items

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
 import { HeadFC, Link, PageProps } from "gatsby";
 import { graphql } from "gatsby";
-import styled from "styled-components";
+import styled, {ThemeProvider} from "styled-components";
 import Layout from "../components/Layout";
 import Seo from "../components/Seo";
 import "../styles.css";
+import {theme} from "../constants/theme";
 
 const StyledMain = styled.main`
   min-width: 500px;
@@ -16,25 +17,64 @@ const StyledMain = styled.main`
   row-gap: 16px;
 `;
 
+const StyledArticle = styled.article`
+  display: flex;
+  flex-direction: row;
+  column-gap: 16px;
+`;
+
+const StyledDivLeft = styled.div`
+  width: 30%;
+  height: 150px;
+  flex-basis: auto;
+  background-color: rgba(0, 0, 0, 20%);
+`;
+
+const StyledDivRight = styled.div`
+  width: 70%;
+  flex-basis: auto;
+  display: flex;
+  flex-direction: column;
+  row-gap: 16px;
+
+  & > p {
+    font-size: 0.8rem;
+  }
+`;
+
+const StyledLink = styled(Link)`
+  color: ${props => props.theme.purple};
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
 const IndexPage: React.FC<PageProps> = ({ data }) => {
   return (
-    <Layout>
-      <StyledMain>
-        <h1>List of My Blogs</h1>
-        {
-          data.allMdx.nodes.map(node => (
-            <article key={node.id}>
-              <h2>
-                <Link to={`/blogs/${node.frontmatter.slug}`}>
-                  {node.frontmatter.title}
-                </Link>
-              </h2>
-              <p>{node.excerpt}</p>
-            </article>
-          ))
-        }
-      </StyledMain>
-    </Layout>
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <StyledMain>
+          <h1>List of My Blogs</h1>
+          {
+            data.allMdx.nodes.map(node => (
+              <StyledArticle key={node.id}>
+                <StyledDivLeft></StyledDivLeft>
+                <StyledDivRight>
+                  <h2>
+                    <StyledLink to={`/blogs/${node.frontmatter.slug}`}>
+                      {node.frontmatter.title}
+                    </StyledLink>
+                  </h2>
+                  <p>{node.excerpt}</p>
+                </StyledDivRight>
+              </StyledArticle>
+            ))
+          }
+        </StyledMain>
+      </Layout>
+    </ThemeProvider>
   )
 }
 
