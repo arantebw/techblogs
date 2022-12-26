5d47418ba48705d8dc303a02c6b402fff00f66ca
Billy Arante <arantebillywilson@gmail.com>
2022-12-26 20:19:50 +0800

    feat(about): ✨ add page content

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,15 +1,28 @@
 import * as React from "react";
-import "../styles.css";
-import Layout from "../components/Layout";
 import { HeadFC, PageProps } from "gatsby";
+import Layout from "../components/Layout";
 import Seo from "../components/Seo";
+import "../styles.css";
+import { StyledMain } from "../styles";
 
 const AboutPage: React.FC<PageProps> = () => {
   return (
     <Layout>
-      <main>
-        <h1>About me</h1>
-      </main>
+      <StyledMain>
+        <h1>About Me</h1>
+        <p>
+          Hi, I'm Billy a nerdy computer programmer, contributes to
+          open-source software projects, works as a freelance full-stack web
+          developer, and maintains my personal tech blog during my leisure
+          hours.
+        </p>
+        <p>
+          If I'm not doing those things mentioned above, I love to watch animes
+          and movies on Netflix, play NBA2K on PS4 gaming console, go on road
+          trips with my motorcycle together with friends, and read books on
+          various genres.
+        </p>
+      </StyledMain>
     </Layout>
   );
 };
--- src/styles.ts
+++ src/styles.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { Link } from "gatsby";
+import { device } from "./constants/theme";
 
 // Styled components accessible to the global scope of the app.
 
@@ -10,5 +11,25 @@ export const StyledLink = styled(Link)`
 
   &:hover {
     border-bottom: 1px solid ${props => props.theme.purple};
+  }
+`;
+
+export const StyledMain = styled.main`
+  display: flex;
+  flex-direction: column;
+  row-gap: 16px;
+  padding: 16px;
+
+  @media ${device.tablet} {
+    padding: 24px;
+  }
+
+  @media ${device.laptop} {
+    max-width: 70%;
+    margin: 0 auto;
+  }
+
+  @media ${device.laptopLarge} {
+    max-width: 60%;
   }
 `;
