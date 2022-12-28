de6834b8ab62c434b2a5b3ea2843ede0c82bce06
Billy Arante <arantebillywilson@gmail.com>
2022-12-28 13:38:45 +0800

    feat(navbar): ✨ add the main navbar

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,15 @@
+import {Link} from "gatsby";
+import * as React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
